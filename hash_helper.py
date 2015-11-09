a89df453760f8ab8c5a8bc6e3cd5b5249bdf3c58
Wes Evans <euphwes@gmail.com>
2015-11-09 10:55:59 -0700

    Added Adler32 and CRC32 checksums as options.

--- hash_helper.py
+++ hash_helper.py
@@ -1,11 +1,14 @@
 import hashlib as hlb
+import zlib as zlb
 import sys
 
 #----------------------------------------------------------------------
 
-hash_flags = ['--md5', '--sha1', '--sha224', '--sha256', '--sha384', '--sha512']
-hash_funcs = [hlb.md5, hlb.sha1, hlb.sha224, hlb.sha256, hlb.sha384, hlb.sha512]
+hash_flags = ['--md5', '--sha1', '--sha224', '--sha256',
+              '--sha384', '--sha512', '--adler32', '--crc32']
 
+hash_funcs = [hlb.md5, hlb.sha1, hlb.sha224, hlb.sha256,
+              hlb.sha384, hlb.sha512, zlb.adler32, zlb.crc32]
 
 function_map = dict(zip(hash_flags, hash_funcs))
 
@@ -13,9 +16,12 @@ function_map = dict(zip(hash_flags, hash_funcs))
 
 def get_hash(contents, which_hash):
 
-    hash_func = function_map[which_hash]
+    hash_result = function_map[which_hash](open(contents, 'rb').read())
 
-    return hash_func(open(contents, 'rb').read()).hexdigest()
+    if which_hash in ('--crc32', '--adler32'):
+        return format(hash_result, 'x')
+
+    return hash_result.hexdigest()
 
 #----------------------------------------------------------------------
 
