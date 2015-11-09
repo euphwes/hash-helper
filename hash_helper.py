b6afeae3d7316d6bb7ca1783a5448015bfca45c9
Wes Evans <euphwes@gmail.com>
2015-11-09 12:31:05 -0700

    Properly implemented file hashing for all of the hashlib hashing functions, and CRC32 and Adler32.

--- hash_helper.py
+++ hash_helper.py
@@ -1,6 +1,6 @@
 import sys
-import zlib as zlb
-import hashlib as hlb
+import zlib as z
+import hashlib as h
 import argparse
 
 #----------------------------------------------------------------------
@@ -12,12 +12,11 @@ class MyParser(argparse.ArgumentParser):
         self.print_help()
         sys.exit(2)
 
-#----------------------------------------------------------------------
 
 def build_parser():
 
     description = "Displays the checksum of either a string literal or a file. "
-    description += "Possible checksum types are md5, sha1, sha224, sha256, sha384, sha512, Adler32, or CRC32."
+    description += "Possible checksum types are md5, sha1, sha224, sha256, sha384, sha512, adler32, or crc32."
     parser = MyParser(description=description)
 
     parser.add_argument('-f', '--file', type=argparse.FileType('r'), required=False)
@@ -28,11 +27,12 @@ def build_parser():
 
     return parser
 
+#----------------------------------------------------------------------
 
 def get_string_hash(target, which_hash):
 
     hash_flags = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'adler32', 'crc32']
-    hash_funcs = [hlb.md5, hlb.sha1, hlb.sha224, hlb.sha256, hlb.sha384, hlb.sha512, zlb.adler32, zlb.crc32]
+    hash_funcs = [h.md5, h.sha1, h.sha224, h.sha256, h.sha384, h.sha512, z.adler32, z.crc32]
     function_map = dict(zip(hash_flags, hash_funcs))
 
     hash_result = function_map[which_hash](target)
@@ -41,6 +41,42 @@ def get_string_hash(target, which_hash):
         return format(hash_result, 'x')
 
     return hash_result.hexdigest()
+
+
+def get_file_hash(target, which_hash):
+
+    hashlib_flags = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512']
+    hashlib_funcs = [h.md5, h.sha1, h.sha224, h.sha256, h.sha384, h.sha512]
+    hashlib_map = dict(zip(hashlib_flags, hashlib_funcs))
+
+    if which_hash == 'adler32':
+        hash_sum = 1
+        with open(target, 'rb') as f:
+            while True:
+                data = f.read(2**20)
+                if not data:
+                    break
+                hash_sum = z.adler32(data, hash_sum)
+                if hash_sum < 0:
+                    hash_sum += 2**32
+            return format(hash_sum, 'x')
+
+    elif which_hash == 'crc32':
+        prev = 0
+        for line in open(target, 'rb'):
+            prev = z.crc32(line, prev)
+        return format(prev & 0xFFFFFFFF, 'x')
+
+    else:
+        func = hashlib_map[which_hash]()
+        target = open(target, 'rb')
+        while True:
+            data = target.read(2**20)
+            if not data:
+                break
+            func.update(data)
+
+        return func.hexdigest()
 
 #----------------------------------------------------------------------
 
@@ -55,5 +91,7 @@ if __name__ == '__main__':
 
     if args.string:
         hashed = get_string_hash(args.string.encode('utf-8'), args.hash)
+    else:
+        hashed = get_file_hash(args.file.name, args.hash)
 
     print('\n{}'.format(hashed))
