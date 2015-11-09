ea333742be1967c71f89b77b50f0242df3c855b2
Wes Evans <euphwes@gmail.com>
2015-11-09 10:57:23 -0700

    Improved argument parsing and handling using argpase from the stdlib. The hash algorithm is now passed as an option to a flag, rather than the flag itself.

--- hash_helper.py
+++ hash_helper.py
@@ -1,22 +1,41 @@
-import hashlib as hlb
-import zlib as zlb
 import sys
+import zlib as zlb
+import hashlib as hlb
+import argparse
 
 #----------------------------------------------------------------------
 
-hash_flags = ['--md5', '--sha1', '--sha224', '--sha256',
-              '--sha384', '--sha512', '--adler32', '--crc32']
+class MyParser(argparse.ArgumentParser):
 
-hash_funcs = [hlb.md5, hlb.sha1, hlb.sha224, hlb.sha256,
-              hlb.sha384, hlb.sha512, zlb.adler32, zlb.crc32]
-
-function_map = dict(zip(hash_flags, hash_funcs))
+    def error(self, message):
+        print(); sys.stderr.write("Error: {}\n".format(message))
+        print(); self.print_help()
+        sys.exit(2)
 
 #----------------------------------------------------------------------
 
-def get_hash(contents, which_hash):
+def build_parser():
 
-    hash_result = function_map[which_hash](open(contents, 'rb').read())
+    description = "Displays the checksum of either a string literal or a file. "
+    description += "Possible checksum types are md5, sha1, sha224, sha256, sha384, sha512, Adler32, or CRC32."
+    parser = MyParser(description=description)
+
+    parser.add_argument('-f', '--file', type=file, required=False)
+    parser.add_argument('-c', '--string', type=str, required=False)
+
+    hash_choices = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'adler32', 'crc32']
+    parser.add_argument('-x', '--hash', type=str, choices=hash_choices, required=True)
+
+    return parser
+
+
+def get_hash(target, which_hash):
+
+    hash_flags = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'adler32', 'crc32']
+    hash_funcs = [hlb.md5, hlb.sha1, hlb.sha224, hlb.sha256, hlb.sha384, hlb.sha512, zlb.adler32, zlb.crc32]
+    function_map = dict(zip(hash_flags, hash_funcs))
+
+    hash_result = function_map[which_hash](target)
 
     if which_hash in ('--crc32', '--adler32'):
         return format(hash_result, 'x')
@@ -27,8 +46,21 @@ def get_hash(contents, which_hash):
 
 if __name__ == '__main__':
 
-    contents = sys.argv[1]
-    which_hash = sys.argv[2]
+    parser = build_parser()
+    args = parser.parse_args()
 
-    print()
-    print(get_hash(contents, which_hash))
+    if not (args.file or args.string):
+        print('\nMust provide either a file or a string literal to be checksummed.')
+        sys.exit(2)
+
+    if not (args.hash):
+        print('\nMust provide which hash function you want to use.')
+
+    if args.file:
+        target = open(args.file, 'rb').read()
+    else:
+        target = args.string
+
+    which_hash = args.hash
+
+    print('\n' + get_hash(target, which_hash))
